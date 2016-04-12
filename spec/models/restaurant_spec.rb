b191a3e5d64021cbc512baf656c55770d5645abb
Misa Ogura <misa.ogura01@gmail.com>
2016-04-12 15:48:20 +0100

    Add validation for the uniqueness of restaurants' names and range of rating 1..5

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -9,4 +9,10 @@ describe Restaurant, type: :model do
     expect(resto).to have(1).error_on(:name)
     expect(resto).not_to be_valid
   end
+
+  it 'is not valid unless it has a unique name' do
+    Restaurant.create(name: 'Unique')
+    resto = Restaurant.create(name: 'Unique')
+    expect(resto).to have(1).error_on(:name)
+  end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 describe Review, type: :model do
   it { is_expected.to belong_to :restaurant }
+
+  it 'cannot be rated above 5' do
+    review = Review.create(thoughts: 'super good', rating: 10)
+    expect(review).to have(1).error_on(:rating)
+  end
 end
