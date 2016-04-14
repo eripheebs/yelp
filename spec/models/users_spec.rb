9cc4a03c9a917f254f2070030c1f38cb41bcfe52
Misa Ogura <misa.ogura01@gmail.com>
2016-04-14 12:23:19 +0100

    Create one-to-many associations between User-Reviewed_restaurants in User model

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 describe User do
   it { is_expected.to have_many :restaurants }
   it { is_expected.to have_many :reviews}
+  it { is_expected.to have_many :reviewed_restaurants }
 end
